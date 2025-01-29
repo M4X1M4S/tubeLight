cc12210e903ed66808dd8620a74420a0deabd1cd
ABHILASH <99235816+M4X1M4S@users.noreply.github.com>
2025-01-30 02:00:07 +0530

    search bar functionality added / debouncing

--- tubelight/src/components/Head.js
+++ tubelight/src/components/Head.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../index.css'
 import { useDispatch } from 'react-redux'
 import { toggleHamburger } from '../Utils/hamburgerSlice'
 
 const Head = () => {
+  const [search,setSearch]=useState('');
+  const[suggestions,setSuggestions]=useState([]);
+  const[showSuggestions, setShowSuggestions]=useState(false);
+ 
+  useEffect(()=>{
+
+    const timer = setTimeout(()=>getSearchSuggestions(),200);
+
+    return()=>{
+      clearTimeout(timer);
+    }
+  },[search])
+
+  const getSearchSuggestions=async()=>{
+      const response = await fetch(`http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=${search}`);
+      
+      const json= await response.json();
+      setSuggestions(json[1]);
+  }
   const dispatch=useDispatch();
+  
   const handleClick =()=>{
     dispatch(toggleHamburger());
   }
@@ -15,9 +35,15 @@ const Head = () => {
         <img className = "h-12 p-2 m-2 hover:cursor-pointer"src='https://static.cdnlogo.com/logos/y/92/youtube.svg' alt='logo' />
         </div>
         <div className='p-2 m-2'>
-          <input className=" w-9/12 border-2 rounded-l-xl" type="text" placeholder="Search" />
+          <input onFocus={()=>setShowSuggestions(true)} onBlur={()=>setShowSuggestions(false)}onChange={(e)=>setSearch(e.target.value)} value={search} className=" w-9/12 border-2 rounded-l-xl" type="text" placeholder="Search" />
           <button className=" border-2 rounded-r-xl">🔍</button>
+          <div className=' w-[32.5rem] rounded-lg shadow-lg border-gray-100 fixed bg-white'>
+          <ul>
+            {showSuggestions&&suggestions.map(s=><li className='p-1 m-1 hover:bg-gray-50'>{s}</li>)}
+          </ul>
         </div>
+        </div>
+       
 
         <div className=''>
           <img className=' h-12 p-2 m-2 ml-auto'src="https://cdn-icons-png.flaticon.com/512/1144/1144760.png" alt='user-logo' />
